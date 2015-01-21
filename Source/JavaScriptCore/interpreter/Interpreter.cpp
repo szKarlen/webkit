700b605c37b748f19fdb56229df175fa49676104
msaboff@apple.com <msaboff@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2015-01-21 21:43:55 +0000

    Eliminate Scope slot from JavaScript CallFrame https://bugs.webkit.org/show_bug.cgi?id=136724
    Reviewed by Geoffrey Garen.
    
    This finishes the removal of the scope chain slot from the call frame header.
    
    * dfg/DFGOSRExitCompilerCommon.cpp:
    (JSC::DFG::reifyInlinedCallFrames):
    * dfg/DFGPreciseLocalClobberize.h:
    (JSC::DFG::PreciseLocalClobberizeAdaptor::readTop):
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::emitCall):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::emitCall):
    * ftl/FTLJSCall.cpp:
    (JSC::FTL::JSCall::emit):
    * ftl/FTLLowerDFGToLLVM.cpp:
    (JSC::FTL::LowerDFGToLLVM::compileNativeCallOrConstruct):
    (JSC::FTL::LowerDFGToLLVM::compileCallOrConstruct):
    * interpreter/JSStack.h:
    * interpreter/VMInspector.cpp:
    (JSC::VMInspector::dumpFrame):
    * jit/JITCall.cpp:
    (JSC::JIT::compileOpCall):
    * jit/JITCall32_64.cpp:
    (JSC::JIT::compileOpCall):
    * jit/JITOpcodes32_64.cpp:
    (JSC::JIT::privateCompileCTINativeCall):
    * jit/Repatch.cpp:
    (JSC::generateByIdStub):
    (JSC::linkClosureCall):
    * jit/ThunkGenerators.cpp:
    (JSC::virtualForThunkGenerator):
    (JSC::nativeForGenerator):
    Deleted ScopeChain slot from JSStack.  Removed all code where ScopeChain was being
    read or set.  In most cases this was where we make JS calls.
    
    * interpreter/CallFrameClosure.h:
    (JSC::CallFrameClosure::setArgument):
    (JSC::CallFrameClosure::resetCallFrame): Deleted.
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::execute):
    (JSC::Interpreter::executeCall):
    (JSC::Interpreter::executeConstruct):
    (JSC::Interpreter::prepareForRepeatCall):
    * interpreter/ProtoCallFrame.cpp:
    (JSC::ProtoCallFrame::init):
    * interpreter/ProtoCallFrame.h:
    (JSC::ProtoCallFrame::scope): Deleted.
    (JSC::ProtoCallFrame::setScope): Deleted.
    * llint/LLIntData.cpp:
    (JSC::LLInt::Data::performAssertions):
    * llint/LowLevelInterpreter.asm:
    * llint/LowLevelInterpreter64.asm:
    Removed the related scopeChainValue member from ProtoCallFrame.  Reduced the number of
    registers that needed to be copied from the ProtoCallFrame to a callee's frame
    from 5 to 4.
    
    * llint/LowLevelInterpreter32_64.asm:
    In addition to the prior changes, also deleted the unused macro getDeBruijnScope.
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@178856 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Conflicts:
    	Source/JavaScriptCore/ChangeLog
    
    Eliminate Scope slot from JavaScript CallFrame

--- Source/JavaScriptCore/interpreter/Interpreter.cpp
+++ Source/JavaScriptCore/interpreter/Interpreter.cpp
@@ -906,7 +906,7 @@ failedJSONP:
     ASSERT(codeBlock->numParameters() == 1); // 1 parameter for 'this'.
 
     ProtoCallFrame protoCallFrame;
-    protoCallFrame.init(codeBlock, scope, JSCallee::create(vm, scope->globalObject(), scope), thisObj, 1);
+    protoCallFrame.init(codeBlock, JSCallee::create(vm, scope->globalObject(), scope), thisObj, 1);
 
     if (LegacyProfiler* profiler = vm.enabledProfiler())
         profiler->willExecute(callFrame, program->sourceURL(), program->lineNo(), program->startColumn());
@@ -969,7 +969,7 @@ JSValue Interpreter::executeCall(CallFrame* callFrame, JSObject* function, CallT
         return throwTerminatedExecutionException(callFrame);
 
     ProtoCallFrame protoCallFrame;
-    protoCallFrame.init(newCodeBlock, scope, function, thisValue, argsCount, args.data());
+    protoCallFrame.init(newCodeBlock, function, thisValue, argsCount, args.data());
 
     if (LegacyProfiler* profiler = vm.enabledProfiler())
         profiler->willExecute(callFrame, function);
@@ -1040,7 +1040,7 @@ JSObject* Interpreter::executeConstruct(CallFrame* callFrame, JSObject* construc
         return throwTerminatedExecutionException(callFrame);
 
     ProtoCallFrame protoCallFrame;
-    protoCallFrame.init(newCodeBlock, scope, constructor, jsUndefined(), argsCount, args.data());
+    protoCallFrame.init(newCodeBlock, constructor, jsUndefined(), argsCount, args.data());
 
     if (LegacyProfiler* profiler = vm.enabledProfiler())
         profiler->willExecute(callFrame, constructor);
@@ -1089,7 +1089,7 @@ CallFrameClosure Interpreter::prepareForRepeatCall(FunctionExecutable* functionE
 
     size_t argsCount = argumentCountIncludingThis;
 
-    protoCallFrame->init(newCodeBlock, scope, function, jsUndefined(), argsCount, args);
+    protoCallFrame->init(newCodeBlock, function, jsUndefined(), argsCount, args);
     // Return the successful closure:
     CallFrameClosure result = { callFrame, protoCallFrame, function, functionExecutable, &vm, scope, newCodeBlock->numParameters(), argumentCountIncludingThis };
     return result;
@@ -1106,7 +1106,6 @@ JSValue Interpreter::execute(CallFrameClosure& closure)
         return jsNull();
 
     StackStats::CheckPoint stackCheckPoint;
-    closure.resetCallFrame();
 
     if (LegacyProfiler* profiler = vm.enabledProfiler())
         profiler->willExecute(closure.oldCallFrame, closure.function);
@@ -1193,7 +1192,7 @@ JSValue Interpreter::execute(EvalExecutable* eval, CallFrame* callFrame, JSValue
     ASSERT(codeBlock->numParameters() == 1); // 1 parameter for 'this'.
 
     ProtoCallFrame protoCallFrame;
-    protoCallFrame.init(codeBlock, scope, JSCallee::create(vm, scope->globalObject(), scope), thisValue, 1);
+    protoCallFrame.init(codeBlock, JSCallee::create(vm, scope->globalObject(), scope), thisValue, 1);
 
     if (LegacyProfiler* profiler = vm.enabledProfiler())
         profiler->willExecute(callFrame, eval->sourceURL(), eval->lineNo(), eval->startColumn());
