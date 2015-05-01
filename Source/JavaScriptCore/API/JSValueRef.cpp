0e7e3f3cf302b44c60e2d8f93defb6bad91e0b97
Karlen Simonyan <szkarlen@gmail.com>
2015-05-01 18:41:39 +0300

    License headers update

--- Source/JavaScriptCore/API/JSValueRef.cpp
+++ Source/JavaScriptCore/API/JSValueRef.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006, 2007 Apple Inc. All rights reserved.
+ * Copyright (C) 2015 Swise Corporation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
